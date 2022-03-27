984b5382fff6271e3ee2d57070eb1a16d93babec
Roy776 <jj21861@gmail.com>
2022-03-27 13:08:20 -0400

    server basic setup

--- Server/index.js
+++ Server/index.js
@@ -2,3 +2,24 @@ const express = require("express")
 const app = express();
 const http = require("http")
 const cors = require("cors")
+const { Server } = require("socket.io");
+
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods:["GET","POST"],
+    },
+})
+
+io.on("connection",
+    (socket)=>{console.log(socket.id);
+     socket.on("disconnect", ()=>{
+         console.log("User disconnected", socket.id)
+     })
+});
+
+server.listen(3001, ()=>{
+    console.log("SERVER RUNNING");
+})
--- WebClient/src/App.js
+++ WebClient/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import bgVideo from './video/bgVideo.mp4'
 import awp from './image/awp.png'
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001"); //connect to server
 
 function App() {
   return (
