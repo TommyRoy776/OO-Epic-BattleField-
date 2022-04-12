5ce25194781b68dd7ffd05343db931dfc2c20ea7
Roy776 <jj21861@gmail.com>
2022-04-11 22:14:42 -0400

    moving logic

--- WebClient/src/components/game.js
+++ WebClient/src/components/game.js
@@ -7,10 +7,11 @@ import TeamB from '../image/survivor-shoot_shotgun_2.png'
 class Player{
     
     constructor(){
-       this.x=0
-       this.y=0
+       this.position = {x:0,y:0}
+       this.velocity = {x:0,y:0}
+       this.size = 80
+       this.rotate =0
        this.image = new Image();
-       this.position = {x:0,y:window.innerHeight/2}
     }
     create(){
         let img = this.image;
@@ -21,14 +22,19 @@ class Player{
             ctx.drawImage(img,0, window.innerHeight/2,80,80);
             
         };
-        this.x = 0;
-        this.y = window.innerHeight/2;
+        this.position.x = 0;
+        this.position.y = window.innerHeight/2;
     }
 
     draw(){
         let canvas = document.getElementById('canvas')
         let ctx = canvas.getContext('2d');
-        ctx.drawImage(this.image,0, window.innerHeight/2,80,80);
+        ctx.drawImage(this.image,this.position.x, this.position.y,80,80);
+    }
+
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
     }
  
 }
@@ -53,8 +59,8 @@ class Bullet{
     update(){
         
         this.draw();
-        this.x += this.velocity.x
-        this.y += this.velocity.y
+        this.x += this.velocity.x*7
+        this.y += this.velocity.y*7
     }
 }
 function Game({charcter}){
@@ -63,6 +69,14 @@ function Game({charcter}){
     //const [bullet,setBullet] = useState([]);
     const bullet = useRef([]);
     const playerRef = useRef(null);
+    const keys = useRef({
+        w:{
+            pressed:false
+        },
+        s:{
+            pressed:false
+        }
+    })
    
     const animate = () =>{
         const canvas = document.getElementById('canvas');
@@ -70,7 +84,17 @@ function Game({charcter}){
         requestAnimationFrame(animate);
         ctx.clearRect(0,0,canvas.width,canvas.height);
         fillCanvas();
-        playerRef.current.draw();
+        playerRef.current.update();
+        if(keys.current.w.pressed && playerRef.current.position.y >= 0){
+            console.log(playerRef.current.position.y)
+            playerRef.current.velocity.y = -2;
+        }else if(keys.current.s.pressed && playerRef.current.position.y+playerRef.current.size <= canvas.height){
+            console.log(playerRef.current.position.y)
+            console.log(`canvas ${canvas.height}`);
+            playerRef.current.velocity.y = 2;
+        }else{
+            playerRef.current.velocity.y = 0;
+        }
         bullet.current.forEach((bul) => {
           bul.update();
          
@@ -85,8 +109,8 @@ function Game({charcter}){
     }
 
      const bulletlistener = (e) =>{
-         const theX = playerRef.current.x+72;
-         const theY =  playerRef.current.y+60;
+         const theX = playerRef.current.position.x+72;
+         const theY =  playerRef.current.position.y+60;
          const angle = Math.atan2(e.clientY - theY,e.clientX - theX)
          console.log(angle)
          const velocity = {
@@ -97,6 +121,32 @@ function Game({charcter}){
         console.log(bullet.current);
          bullet.current = [...bullet.current,new Bullet(theX,theY,4,velocity)];
         
+     }
+
+     const move = (e) =>{
+        switch(e.key){
+          case 'w':
+              keys.current.w.pressed = true;
+              break;
+          case 's':
+            keys.current.s.pressed = true;
+              break;
+        }
+     }
+
+     const stopMove = (e) =>{
+        switch(e.key){
+          case 'w':
+              
+              playerRef.current.velocity.y = 0;
+              keys.current.w.pressed = false;
+              break;
+          case 's':
+      
+            playerRef.current.velocity.y = 0;
+            keys.current.s.pressed = false;
+              break;
+        }
      }
 
 
@@ -111,30 +161,15 @@ function Game({charcter}){
          canvas.height = window.innerHeight;
          //setBullet(pre => [...pre,thebullet]);
          canvas.addEventListener("click", bulletlistener);
+         window.addEventListener('keydown',move);
+         window.addEventListener('keyup',stopMove);
          fillCanvas();
          animate();
         
 
     },[])
     
- /*  useEffect(() => {
-        console.log(player);
-        console.log(bullet);
-      }, [player,bullet]);*/
-      
 
-/*useEffect(() => {
-      const canvas = document.getElementById('canvas')
-      canvas.addEventListener("click", listener);
-      return () => canvas.removeEventListener("click", listener);
-    },[player])*/
-   
-    /*useEffect(() => {
-        const canvas = document.getElementById('canvas')
-        canvas.addEventListener("click", bulletlistener);
-        return () => canvas.removeEventListener("click", bulletlistener);
-      },[bullet,player])*/
-   
   
     return( 
     <canvas id="canvas">
